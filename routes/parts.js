1659031cd70ffede2048b2795ba3627c09fce98d
gaetan <gaetan.bloch@gmail.com>
2017-09-11 20:04:42 +0200

    rest application

--- routes/parts.js
+++ routes/parts.js
@@ -14,5 +14,71 @@ router.get('/parts', function (req, res, next) {
 });
 
 // Get single part
+router.get('/part/id', function (req, res, next) {
+    db.parts.findOne({_id: mongojs.ObjectID(req.params.id)},function (err, part) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(part);
+    });
+});
+
+// Save part
+router.post('/part', function (red, res, next) {
+    var part = req.body;
+    if(!part.title || part.isDone + '') {
+        res.status(400);
+        res.json({
+            "error": "bad data"
+        });
+    } else {
+        db.parts.save(part, function (err, part) {
+            if(err){
+                res.send(err);
+            }
+            res.json(part);
+        })
+    }
+});
+
+
+// Delete a part
+router.delete('/part/id', function (req, res, next) {
+    db.parts.remove({_id: mongojs.ObjectID(req.params.id)},function (err, part) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(part);
+    });
+});
+
+
+// Update a part
+router.put('/part/id', function (req, res, next) {
+    var part = req.body;
+    var updPart = {};
+
+    if (part.isDone) {
+        updPart.isDone = part.isDone;
+    }
+
+    if (part.title) {
+        updPart.title = part.title;
+    }
+
+    if (!updPart) {
+        res.status(400);
+        res.json({
+            "error": "bad data"
+        });
+    } else {
+        db.parts.update({_id: mongojs.ObjectID(req.params.id)}, updPart, {}, function (err, part) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(part);
+        });
+    }
+});
 
 module.exports = router;
