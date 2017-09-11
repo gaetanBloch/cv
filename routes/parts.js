4ec69cbb35d6bf14c8c9f1addfc038ecd26bdac3
gaetan <gaetan.bloch@gmail.com>
2017-09-11 14:33:16 +0200

    return data from mongoDB

--- routes/parts.js
+++ routes/parts.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs("mongodb://gaetanbloch:onlov3000@ds133004.mlab.com:33004/mycv", ['parts']);
 
+// Get all parts
 router.get('/parts', function (req, res, next) {
-    res.send('parts');
+    db.parts.find(function (err, parts) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(parts);
+    });
 });
+
+// Get single part
 
 module.exports = router;
